ca0d11c0d8929e9498cbb29e57ee63ca4a765e49
Akshay Gaur <akshay.gaur028@gmail.com>
2024-12-12 22:16:25 +0530

    Steps Components created

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../Components/Header";
+import Steps from "../Components/Steps";
 
 const Home = () => {
   return(
     <div>
       <Header/>
+      <Steps/>
     </div>
   )
 };
